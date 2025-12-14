7b137887d12913d661b547986736bedd590f3e2e
Samu Lang <langsamu@hotmail.com>
2025-12-14 13:15:48 +0000

    Ignore namespace warning on Wrapping

---
+++ Wrapping/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "This library is intended for inclusion in dotNetRDF.", Scope = "namespace", Target = "~N:VDS.RDF.Wrapping")]
